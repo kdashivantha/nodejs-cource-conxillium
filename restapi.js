0cd4dc8e6e377615f4f2e086f0eee84944d17d7f
Amith Shivantha <kdashivantha@gmail.com>
2019-09-05 16:52:32 +0530

    create operation in REST api

--- restapi.js
+++ restapi.js
@@ -3,6 +3,15 @@ const app = express();
 
 const { db } = require('./db');
 
+//body parser maps incoming data to req.body,
+//so we can access JSON data fromreq.body
+const bodyParser = require('body-parser');
+
+// Parse incoming requests data
+// apply as a middleware for the incomming channel
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req,res)=>{
     res.send('hello peeps!');
 });
@@ -14,7 +23,35 @@ app.get('/api/todos', (req, res) => {
       message: 'todos retrieved successfully',
       todos: db
     })
-  });
+});
+
+//create
+app.post('/api/todos', (req, res) => {
+    if(!req.body.title) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'title is required'
+      });
+    } else if(!req.body.description) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'description is required'
+      });
+    }
+   const todo = {
+     id: db.length + 1,
+     title: req.body.title,
+     description: req.body.description
+   }
+   db.push(todo);
+   return res.status(201).send({
+     success: 'true',
+     message: 'todo added successfully',
+     todo
+   })
+});
+//use post-man to show
+
 
 const PORT = 5000;
 app.listen(PORT, () =>{
