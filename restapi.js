1a8aa59fc8db0802c46173979e5015290d3e7a6f
Amith Shivantha <kdashivantha@gmail.com>
2019-09-05 16:55:43 +0530

    delete todo

--- restapi.js
+++ restapi.js
@@ -72,6 +72,34 @@ app.get('/api/todos/:id', (req, res) => {
   });
 
 
+  //delete todo
+  app.delete('/api/v1/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+  
+    db.map((todo, index) => {
+      if (todo.id === id) {
+         db.splice(index, 1);
+         return res.status(200).send({
+           success: 'true',
+           message: 'Todo deleted successfuly',
+         });
+      }
+    });
+  
+  
+      return res.status(404).send({
+        success: 'false',
+        message: 'todo not found',
+      });
+  
+   
+  });
+
+
+
+
+  
+
 const PORT = 5000;
 app.listen(PORT, () =>{
     console.log(`listing on port ${PORT}`);
