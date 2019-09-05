54e34ac347befca7d66323bb9655feb56d53d62e
Amith Shivantha <kdashivantha@gmail.com>
2019-09-05 16:04:59 +0530

    core modules examples added

--- core.js
+++ core.js
@@ -1,1 +1,35 @@
 console.log('Hello Conxils');
+/*
+//os module
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Memory Status ${freeMemory}:${totalMemory}`);
+
+//fs module
+
+const fs = require('fs');
+const files = fs.readdirSync('./');
+console.log(files);
+
+//http module
+
+const http = require('http');
+const server = http.createServer( (req,res)=>{
+    if(req.url === '/'){
+        res.write('Its Working');
+        res.end();
+    }
+
+    if(req.url === '/api/users'){
+        res.write(JSON.stringify(['amith', 'amith2', 'amith3'])) ; // :)
+        res.end();
+    }
+});
+
+server.listen(3000);
+console.log('Server is Listing on port 3000');
+
+*/
